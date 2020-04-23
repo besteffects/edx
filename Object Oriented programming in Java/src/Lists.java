c081f1be637dbaf062b5bb4555cc698fb4aeec4a
Vit <besteffects@gmail.com>
2020-04-23 04:56:59 -0400

    Lists

---
+++ Object Oriented programming in Java/src/Lists.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Lists {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<String>();
+        myList.add("text1");
+        myList.add("text2");
+        myList.add("text3");
+        myList.add("text4");
+        System.out.println("Before removal: "+myList);
+        for (int i = 0; i < myList.size(); i++) {
+            myList.remove(i);
+        }
+        System.out.println("After removal: " + myList);
+    }
+}
