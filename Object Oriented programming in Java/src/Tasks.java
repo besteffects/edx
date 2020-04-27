bb45dd55c2c41673ec8e77d958be71d029ea7fcd
Vit <besteffects@gmail.com>
2020-04-27 03:02:54 -0400

    2d arrays assessment task

--- Object Oriented programming in Java/src/Tasks.java
+++ Object Oriented programming in Java/src/Tasks.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Tasks {
-    /*
-    Assume that a two-dimensional rectangular array of integers called data has been declared with four rows and seven columns.
-     Write a for loop to initialize the third row of data to store the numbers 1 through 7.
-     */
+
 
     public static void main(String[] args) {
+        /* 1 Assume that a two-dimensional rectangular array of integers called data has been declared with four rows and seven columns.
+     Write a for loop to initialize the third row of data to store the numbers 1 through 7.
+     */
         initializeTwoDLoop();
+
+        //2
         ArrayList<Integer> mystery3List = new ArrayList<Integer>();
         mystery3List.add(10);
         mystery3List.add(20);
@@ -16,6 +18,23 @@ public class Tasks {
         mystery3List.add(40);
         mystery3(mystery3List);
         mystery3(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+        /*3  Write a method removeEvenLength that takes an ArrayList of Strings as a parameter and that removes all of the
+        strings of even length from the list.
+         */
+        ArrayList<String> stringList = new ArrayList<String>() {
+            {
+                add("One");
+                add("Two");
+                add("Three");
+                add("Four");
+                add("Five");
+                add("1111");
+                add("11");
+                add("big");
+            }
+        };
+        removeEvenLength(new ArrayList<String>(stringList));
     }
 
 
@@ -39,6 +58,16 @@ public class Tasks {
             list.set(i, a + b);
         }
         System.out.println("Show list: " + list);
+    }
+
+    static void removeEvenLength(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if ((list.get(i)).length() % 2 == 0) {
+                list.remove(i); //list becomes shorter, so decrease of index variable after removal is required
+                i--;
+            }
+        }
+        System.out.println(list);
     }
 
 }
---
+++ Object Oriented programming in Java/src/lists/Assessment.java
@@ -0,0 +1,27 @@
+package lists;
+
+import java.util.Arrays;
+
+public class Assessment {
+
+
+    public static void main(String[] args) {
+        // 1)  2d array
+        twoDimensions(new int[][]{{44, 45, 16, 50},
+                {16, 12, 66, 9},
+                {52, 83, 3, 37},
+                {56, 61, 69, 61}});
+    }
+
+    public static void twoDimensions(int[][] numbers){
+        for (int i = 0; i < numbers.length - 1; i++){
+            for (int j = 0; j < numbers[i].length - 1; j++){
+                if (numbers[i][j] < numbers[i][j + 1]){
+                    numbers[i][j] = numbers[i][j] + numbers[i + 1][j];
+                    numbers[i + 1][j] = numbers[i + 1][j] / 2;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(numbers));
+    }
+}
