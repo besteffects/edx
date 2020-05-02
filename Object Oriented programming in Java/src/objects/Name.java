95e3a46d519b65774de8a0f46a0b167966d58754
Vit <besteffects@gmail.com>
2020-05-02 02:50:01 -0400

    Encapsulation

--- Object Oriented programming in Java/src/objects/Name.java
+++ Object Oriented programming in Java/src/objects/Name.java
@@ -1,7 +1,25 @@
 package objects;
 
 public class Name {
-    String firstName;
-    String lastName;
-    char middleInitial;
+    private String firstName;
+    private String lastName;
+    private char middleInitial;
+
+    public Name(String first, char middle, String last) {
+        firstName = first;
+        middleInitial = middle;
+        lastName = last;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public char getMiddleInitial() {
+        return middleInitial;
+    }
 }
