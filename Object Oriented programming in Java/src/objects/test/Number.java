58ae333d7566ef2fe04da5219eba6586e4ed44aa
Vit <besteffects@gmail.com>
2020-05-03 02:03:59 -0400

    Test2

---
+++ Object Oriented programming in Java/src/objects/test/Number.java
@@ -0,0 +1,19 @@
+package objects.test;
+
+public class Number {
+    private int value;
+
+    public Number() {
+        value = 0;
+    }
+
+    public Number(int num) {
+        value = num;
+    }
+    public void setVal(int n){
+        value=n;
+    }
+    public String toString(){
+        return ("Number: " + value);
+    }
+}
---
+++ Object Oriented programming in Java/src/objects/test/NumberTest.java
@@ -0,0 +1,8 @@
+package objects.test;
+// A new number object, firstNum, is declared and instantiated with a value of 0
+public class NumberTest{
+    public static void main(String[] args) {
+        Number firtsNum= new Number();
+        System.out.println(firtsNum);
+    }
+}
