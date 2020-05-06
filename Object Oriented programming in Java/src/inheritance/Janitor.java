ce30fdd5a7260dc1bb153f961ae471c73d6e472f
Vit <besteffects@gmail.com>
2020-05-06 17:27:59 -0400

    Inheritance. Super task. Staff

---
+++ Object Oriented programming in Java/src/inheritance/Janitor.java
@@ -0,0 +1,4 @@
+package inheritance;
+
+public class Janitor extends Staff {
+}
---
+++ Object Oriented programming in Java/src/inheritance/Staff.java
@@ -0,0 +1,31 @@
+package inheritance;
+
+public class Staff {
+    private int hours=80;
+    private int salary=40000;
+    private int vacation=10;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getVacation() {
+        return vacation;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setVacation(int vacation) {
+        this.vacation = vacation;
+    }
+}
