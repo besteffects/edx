ddfa1a19d5be05597bf2bf6622b552b0d0b0b92e
Vit <besteffects@gmail.com>
2020-05-07 03:21:34 -0400

    Inheritance. Super task. Car classes

---
+++ Object Oriented programming in Java/src/inheritance/MonsterTruck.java
@@ -0,0 +1,26 @@
+package inheritance;
+
+public class MonsterTruck extends Car {
+    public void m1() {
+        System.out.println("monster 1");
+    }
+
+    public void m2() {
+        System.out.println("truck 1");
+        super.m1();
+
+    }
+    public String toString() {
+        return "monster "+super.toString()+super.toString();
+    }
+
+    public static void main(String[] args) {
+        Car car= new Car();
+        MonsterTruck ms = new MonsterTruck();
+//        Truck track = new Truck();
+        ms.m1();
+        ms.m2();
+
+
+    }
+}
