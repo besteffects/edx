c7e033d0a494e497b73b23e653880b0541da0a68
Vit <besteffects@gmail.com>
2020-05-06 23:10:40 -0400

    Inheritance. Super task. Janitor completed

--- Object Oriented programming in Java/src/inheritance/Janitor.java
+++ Object Oriented programming in Java/src/inheritance/Janitor.java
@@ -1,20 +1,28 @@
 package inheritance;
 
-public class Janitor extends Staff {
-    public Janitor(int hours, int salary, int vacation){
-        super(hours, salary, vacation);
+public class Janitor extends Employee {
+//    private int hours;
+//    private double salary;
+//    private int vacation;
+
+//    public Janitor(int hours, int salary, int vacation) {
+//        super(hours, salary, vacation);
+//    }
+
+    public int getHours() {
+        return super.getHours() * 2;
     }
 
-    public int getHours(){
-        return super.getHours()*2;
+    public double getSalary() {
+        return super.getSalary() - 10000;
     }
-    public int getSalary(){
-        return super.getSalary()-10000;
+
+    public int getVacationDays() {
+        return  super.getVacationDays()/ 2;
     }
-    public int getVacation(){
-        return super.getVacation()-5;
-    }
-    public void clean(){
+
+    public void clean() {
         System.out.println("Workin' for the man.");
     }
+
 }
