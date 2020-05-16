623883821490c920f40a85584429e8401b53d24f
Vit <besteffects@gmail.com>
2020-05-16 03:18:34 -0400

    Employee super class

--- Object Oriented programming in Java/src/lists_arrays/companyStructure/Employee.java
+++ Object Oriented programming in Java/src/lists_arrays/companyStructure/Employee.java
@@ -1,8 +1,11 @@
 package lists_arrays.companyStructure;
 
-public class Employee {
+abstract public class Employee {
     private String name;
     private double baseSalary;
+    private int employeeID;
+    private Employee manager;
+    private String status;
 
     //Should construct a new employee object and take in two parameters, one for the name of the user and one for their base salary
     public Employee(String name, double baseSalary) {
@@ -10,11 +13,39 @@ public class Employee {
         this.baseSalary = baseSalary;
     }
 
-    public double getBaseSalary(){
+    public double getBaseSalary() {
         return baseSalary;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
+
+    /*
+    Should return the employee's ID. The ID should be issued on behalf of the employee at the time they are constructed.
+    The first ever employee should have an ID of "1", the second "2" and so on
+     */
+    public int getEmployeeID() {
+        return employeeID = employeeID++;
+    }
+
+    //Should return a reference to the Employee object that represents this employee's manager
+    public Employee getManager() {
+        return manager;
+    }
+
+    // Should return true if the two employee IDs are the same, false otherwise
+    public boolean equals(Employee other) {
+        return this.employeeID == other.employeeID;
+    }
+
+    //Should return a String representation of the employee that is a combination of their id followed by their name.
+// Example: "1 Kasey"
+    public String toString() {
+        return this.employeeID + " " + this.name;
+    }
+
+    // Should return a String representation of that Employee's current status. This will be different for every
+    // subclass of Employee
+    abstract public String employeeStatus();
 }
