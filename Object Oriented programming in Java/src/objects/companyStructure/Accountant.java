c7c9546e8c3aade39631a9fa7fdbdb81bace0176
Vit <besteffects@gmail.com>
2020-05-19 03:48:31 -0400

    Added Accountant

--- Object Oriented programming in Java/src/objects/companyStructure/Accountant.java
+++ Object Oriented programming in Java/src/objects/companyStructure/Accountant.java
@@ -1,4 +1,17 @@
 package objects.companyStructure;
 
-public class Accountant {
+public class Accountant extends BusinessEmployee {
+    TechnicalLead lead;
+
+    //TODO Should start with a bonus budget of 0 and no team they are officially supporting
+    public Accountant(String name) {
+        super(name);
+
+    }
+
+    //TODO Should return a reference to the TechnicalLead that this Accountant is currently supporting. If they have
+    // not been assigned a TechnicalLead null should be returned
+    public TechnicalLead getTeamSupported(){
+        return lead;
+    }
 }
