25e3e45323fc621e1b24e9e6fa0a5a1b2daa0f70
Vit <besteffects@gmail.com>
2020-05-24 17:19:35 -0400

    TechnicalLead

--- Object Oriented programming in Java/src/objects/companyStructure/TechnicalLead.java
+++ Object Oriented programming in Java/src/objects/companyStructure/TechnicalLead.java
@@ -1,7 +1,10 @@
 package objects.companyStructure;
 
+import java.util.ArrayList;
+
 public class TechnicalLead extends TechnicalEmployee {
     int headCount;
+    ArrayList<Accountant> directReport = new ArrayList<>();
 
     //Should create a new TechnicalLead that is a Manager. The TechnicalLead's base salary should be 1.3 times that
 // of a TechnicalEmployee. TechnicalLeads should have a default head count of 4.
@@ -10,4 +13,11 @@ public class TechnicalLead extends TechnicalEmployee {
         setBaseSalary(getBaseSalary() * 1.3); //base salary can be protected in head class
         headCount = 4;
     }
+
+    //Should return true if the number of direct reports this manager has is less than their headcount.
+    public boolean hasHeadCount() {
+        return directReport.size() < headCount;
+    }
+
 }
+
