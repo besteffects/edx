efcacbf81bd1200d6c3f3db7461aa2a610528c29
Vit <besteffects@gmail.com>
2020-06-01 23:02:01 -0400

    Accountant. approveBonus

--- Object Oriented programming in Java/src/objects/companyStructure/Accountant.java
+++ Object Oriented programming in Java/src/objects/companyStructure/Accountant.java
@@ -21,22 +21,22 @@ public class Accountant extends BusinessEmployee {
     // should be updated to be the total of each SoftwareEngineer's base salary that reports to that TechnicalLead plus 10%.
     // For example, if the TechnicalLead supports 2 SoftwareEngineers, each with a salary of 75000, the Accountant's budget should be 150000 + 15000 for a total of 165000
     public void supportTeam(TechnicalLead leadRef) {
-        this.lead=leadRef;
-        double total=0;
-        for (int i = 0; i< lead.getDirectReport().size();i++) {
-            total+=lead.getDirectReport().get(i).getBaseSalary();
+        this.lead = leadRef;
+        double total = 0;
+        for (int i = 0; i < lead.getDirectReport().size(); i++) {
+            total += lead.getDirectReport().get(i).getBaseSalary();
         }
-        setBonusBudget(total*1.1);
+        setBonusBudget(total * 1.1);
     }
 
-    //TODO Should take in a suggested bonus amount and check if there is still enough room in the budget. If the bonus is greater
+    //TODO (TO VERIFY) Should take in a suggested bonus amount and check if there is still enough room in the budget. If the bonus is greater
 // than the remaining budget, false should be returned, otherwise true. If the accountant is not supporting any team
 // false should be returned.
     public boolean approvedBonus(double bonus) {
-        return true;
+        return (bonus > getBonusBudget() || lead == null);
     }
 
-    //TODO Should return a String representation of this Accountant that includes their ID, name, the size of their currently
+    //TODO (TO VERIFY)Should return a String representation of this Accountant that includes their ID, name, the size of their currently
     // managed budget and the name of the TechnicalLead they are currently supporting. Example: "1 Kasey with a budget of
     // 22500.0 is supporting Satya Nadella"
     public String employeeStatus() {
